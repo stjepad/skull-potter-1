2e5ca52f94469cb2b8d2724d8e6db0412bb429e7
Dejan Stjepanovic <ds2q88@gmail.com>
2018-05-09 21:47:51 -0500

    Works, but has more work needed

--- src/scripts/dom/CommissionWork.js
+++ src/scripts/dom/CommissionWork.js
@@ -1,20 +1,115 @@
 // Purpose: Build the commission form
+const $ = require("jquery")
+const api = require("../api/APIManager")
 
-const buildCommissionForm = function () {
-    // Customer info (name, address, phone number, email)
+const buildCommissionForm = function (id) {
 
-    // Type of animal
+    // What customer selected
+    api.getSingleAnimal(id).then(animal => {
+        const output = $("#commissionWork")
+        const fragment = document.createDocumentFragment()
 
-    // Quantity of animals
+        // Customer info (name, address, phone number, email)
 
-    // Size
+        const inputFieldFactory = (classlist, defaultPlaceHolder, type) => {
+
+            const inputField = document.createElement("input")
+            inputField.setAttribute("type", type)
+            inputField.classList = classlist
+            inputField.placeholder = defaultPlaceHolder
+            return inputField}
+
+        const newTaskSection = document.querySelector("#inputInfo")
+
+
+        const inputContainer = document.createElement("div")
+        inputContainer.classList = "inputForm"
+
+        //First Name
+        const firstName = inputFieldFactory("form--firstName", "First Name", "text")
+        inputContainer.appendChild(firstName)
+        // Last Name
+        const lastName = inputFieldFactory("form--lastName", "Last Name", "text")
+        inputContainer.appendChild(lastName)
+        // address ()
+        const address = inputFieldFactory("form--address", "Address", "text")
+        inputContainer.appendChild(address)
+        // phone number
+        const phoneNumber = inputFieldFactory("form--phoneNumber", "Phone Number", "text")
+        inputContainer.appendChild(phoneNumber)
+        // email
+        const email = inputFieldFactory("form--email", "Customer E-mail", "text")
+        inputContainer.appendChild(email)
+        // Type of animal
+        const typeOfAnimal = inputFieldFactory("form--typeOfAnimal", "Type of Animal", "text")
+        inputContainer.appendChild(typeOfAnimal)
+        // Quantity of animals
+        const quantityContainer = document.createElement("div")
+        quantityContainer.classList = "form-group"
+
+        const quantityLabel = document.createElement("label")
+        quantityLabel.textContent = "Quantity"
+
+        const quantity = document.createElement("input")
+        quantity.classList = "form-control"
+        quantity.type = "number"
+        quantity.min = 1
+        quantity.max = 10
+        quantityContainer.appendChild(quantityLabel)
+        quantityContainer.appendChild(quantity)
+        inputContainer.appendChild(quantityContainer)
+
+        // Size
+
+        const sizeContainer = document.createElement("div")
+        sizeContainer.classList = "form-group"
+
+        const quantityLabel = document.createElement("label")
+        quantityLabel.textContent = "Quantity"
+
+        const quantity = document.createElement("input")
+        quantity.classList = "form-control"
+        quantity.type = "size"
+        quantity.min = 1
+        quantity.max = 10
+        sizeContainer.appendChild(quantityLabel)
+        sizeContainer.appendChild(quantity)
+        inputContainer.appendChild(sizeContainer)
+        // Submit button
+
+        const btn = document.createElement("BUTTON");
+        let t = document.createTextNode("Submit Button");
+        btn.appendChild(t);
+        document.body.appendChild(btn);
+
+        const x = document.createElement("INPUT");
+        x.setAttribute("type", "checkbox");
+        let checkBoxText = document.createTextNode("blah");
+        document.body.appendChild(x)
+        x.appendChild(checkBoxText);
+        newTaskSection.appendChild(x)
+
+
+
+        newTaskSection.appendChild(inputContainer)
+        output.append(fragment)
+
+
+
+    })
+
+
+
+
+
+
+
 
     // Mounted?
 
     // When you want it by (date field)
+
+
 }
-
-
-
 
 module.exports = buildCommissionForm
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,10 @@
-// Display a list of all animals
-const listAnimals = require("./dom/DisplayCatalog")
-listAnimals()
+// const listAnimals = require("./dom/DisplayCatalog")
+const orderForm = require("./dom/OrderForm")
+const commissionWork = require("./dom/CommissionWork")
 
+// Display a list of all animals
+// listAnimals()
+
+// Display order form
+// orderForm(1)
+commissionWork(1)
